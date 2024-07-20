08d21ece44f63f5b00b257e5c0b3f46c4e4840cd
ajax <ajax-dong@outlook.com>
2024-07-20 16:57:08 +0800

    Added fields to control when an announcement should be shown

--- ThuInfoWeb/DBModels/Announce.cs
+++ ThuInfoWeb/DBModels/Announce.cs
@@ -7,12 +7,23 @@ namespace ThuInfoWeb.DBModels
     {
         [Column(IsIdentity = true, IsPrimary = true)]
         public int Id { get; set; }
+
         public string Title { get; set; }
+
         [Column(StringLength = -1)]
         public string Content { get; set; }
+
         public string Author { get; set; }
+
         public DateTime CreatedTime { get; set; }
+
         [JsonIgnore]
         public bool IsActive { get; set; }
+
+        [Column(StringLength = 10, IsNullable = false)]
+        public string VisibleNotAfter { get; set; }
+
+        [Column(StringLength = 30, IsNullable = false)]
+        public string VisibleExact { get; set; }
     }
 }
--- ThuInfoWeb/Models/AnnounceViewModel.cs
+++ ThuInfoWeb/Models/AnnounceViewModel.cs
@@ -5,12 +5,23 @@ namespace ThuInfoWeb.Models
     public class AnnounceViewModel
     {
         [Required, Display(Name = "标题")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
+
         [Required, Display(Name = "内容")]
-        public string Content { get; set; }
+        public string? Content { get; set; }
+
         public int Id { get; set; }
-        public string Author { get; set; }
+
+        public string? Author { get; set; }
+
         public DateTime CreatedTime { get; set; }
+
         public bool IsActive { get; set; } = true;
+
+        [Display(Name = "对此版本及之前版本可见")]
+        public string? VisibleNotAfter { get; set; }
+
+        [Display(Name = "对这些版本可见 (使用逗号分隔)")]
+        public string? VisibleExact { get; set; }
     }
 }
