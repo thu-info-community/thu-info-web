1b6c25a789a80740b0832cf9237c097b09c87b66
ajax <ajax-dong@outlook.com>
2025-06-17 15:04:04 +0800

    Added version filter for announcements

--- ThuInfoWeb/Extension.cs
+++ ThuInfoWeb/Extension.cs
@@ -19,4 +19,31 @@ public static partial class Extension
     {
         return VersionRegex().IsMatch(s);
     }
+
+    /// <summary>
+    ///   Compares this version with another version.
+    /// </summary>
+    /// <param name="v">Current version number</param>
+    /// <param name="other">Other version number</param>
+    /// <returns>true if the current version number is greater than the other one</returns>
+    /// <exception cref="ArgumentException">if either part is not a valid version number</exception>
+    public static bool VersionGreaterThan(this string v, string other)
+    {
+        if (!v.IsValidVersionNumber() || !other.IsValidVersionNumber())
+            throw new ArgumentException("Invalid version number format.");
+
+        var vParts = v.Split('.').Select(int.Parse).ToArray();
+        var oParts = other.Split('.').Select(int.Parse).ToArray();
+
+        for (var i = 0; i < 3; i++)
+        {
+            var vPart = i < vParts.Length ? vParts[i] : 0;
+            var oPart = i < oParts.Length ? oParts[i] : 0;
+
+            if (vPart > oPart) return true;
+            if (vPart < oPart) return false;
+        }
+
+        return false;
+    }
 }
